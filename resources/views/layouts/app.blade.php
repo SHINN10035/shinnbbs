6d9a2e11247f1bb83006e2450282389b6d601081
jinlong2016 <2593327338@qq.com>
2018-03-23 10:09:13 +0800

    add captcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'ShinnBBS') - Laravel 进阶教程</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
